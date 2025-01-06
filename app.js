a11dc57669294cff4b507d61d34a932415ad9d0b
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-06 13:42:32 -0300

    criando funcionalidade de reiniciar partida após achar número secreto

--- app.js
+++ app.js
@@ -1,30 +1,53 @@
-
 let nMax = 30;
 let nMin = 1;
-let numeroSecreto = gerarNumeroAleatorio();
-console.log(numeroSecreto);
 
-function atribuirValorCampo(tag, texto){
+startPartida();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+
+    if (chute == numeroSecreto) {
+
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa. UHUU. PARABÉNS!!!! ";
+        atribuirValorCampo('h1', 'Parabéns! Acertou!');
+        atribuirValorCampo('p', `Você descobriu o número secreto em ${tentativas} ${palavraTentativa}`.toString());
+
+        //habilitando o botão de reiniciar após acertar número
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    
+    } else if (chute > numeroSecreto) {
+        atribuirValorCampo('p', `O número secreto é menor que: ${chute}`.toString());
+    } else {
+        atribuirValorCampo('p', `O número secreto é maior que: ${chute}`.toString());
+    }
+    tentativas++;
+    limparCampo();
+
+}
+
+function atribuirValorCampo(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-atribuirValorCampo('h1', 'NumberSecret-Game');
-atribuirValorCampo('p', `Escolha um número entre ${nMin} e ${nMax}`);
-
-
-function verificarChute(){
-    let chute = document.querySelector('input').value;
-    if(chute == numeroSecreto){
-        atribuirValorCampo('h1', 'Parabéns! Acertou!');
-        atribuirValorCampo('p', `Você descobriu o número secreto!`);
-    } else if (chute > numeroSecreto){
-        atribuirValorCampo('p', `O número secreto é menor que: ${chute}`);
-    } else {
-        atribuirValorCampo('p', `O número secreto é maior que: ${chute}`);
-    }
+function gerarNumeroAleatorio() {
+    return Math.floor(Math.random() * (nMax - nMin + 1)) + nMin;
 }
 
-function gerarNumeroAleatorio(){
-    return Math.floor(Math.random() * (nMax - nMin + 1)) + nMin;
+function limparCampo(){
+    document.querySelector('input').value = '';
+}
+
+function startPartida(){
+    tentativas = 1;
+    numeroSecreto = gerarNumeroAleatorio();
+    console.log(numeroSecreto);
+    atribuirValorCampo('h1', 'NumberSecret-Game');
+    atribuirValorCampo('p', `Escolha um número entre ${nMin} e ${nMax}`.toString());
+}
+
+function reiniciarJogo() {
+    limparCampo();
+    startPartida();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
 }
