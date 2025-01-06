7764a5c71e87c97a3755cbeb2a77ee62185e8f21
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-06 11:23:15 -0300

    Adicionando paragráfo e números min e max para intervalo

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
+
+let nMax = 0;
+let nMin = 0;
+
 let titulo = document.querySelector('h1');
 titulo.innerHTML = 'NumberSecret-Game';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = `Escolha um número entre ${nMin} e ${nMax}`;
+
