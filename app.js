4c7e8600d87a6c20729d37f25f826b3ae34cfbcd
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-06 12:55:32 -0300

    criando função para gerar número aleatório e função para verificar o chute

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
-let nMax = 0;
-let nMin = 0;
+let nMax = 30;
+let nMin = 1;
+let numeroSecreto = gerarNumeroAleatorio();
+console.log(numeroSecreto);
 
 function atribuirValorCampo(tag, texto){
     let campo = document.querySelector(tag);
@@ -11,3 +13,18 @@ atribuirValorCampo('h1', 'NumberSecret-Game');
 atribuirValorCampo('p', `Escolha um número entre ${nMin} e ${nMax}`);
 
 
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    if(chute == numeroSecreto){
+        atribuirValorCampo('h1', 'Parabéns! Acertou!');
+        atribuirValorCampo('p', `Você descobriu o número secreto!`);
+    } else if (chute > numeroSecreto){
+        atribuirValorCampo('p', `O número secreto é menor que: ${chute}`);
+    } else {
+        atribuirValorCampo('p', `O número secreto é maior que: ${chute}`);
+    }
+}
+
+function gerarNumeroAleatorio(){
+    return Math.floor(Math.random() * (nMax - nMin + 1)) + nMin;
+}
